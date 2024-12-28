5026a0cdc952bf9b783fe0dc282b1274473e5626
Daria Korchunova <bg.korchunova@gmail.com>
2024-12-28 15:06:59 +0300

    color fix

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Calc-app-vue3',
   plugins: [
     vue(),
     vueDevTools(),
